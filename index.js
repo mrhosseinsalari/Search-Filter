ce0948fb28de098ef7375a21dfa6dda76f95c848
Hossein Salari <hosein.salari81@gmail.com>
2023-04-09 22:04:06 +0330

    render filtered products on DOM

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const searchInput = document.querySelector("#search");
+const productsDOM = document.querySelector(".products-center");
+const btns = document.querySelectorAll(".btn");
+
 let allProductsData = [];
 
 const filters = {
@@ -21,11 +24,35 @@ function renderProducts(_products, _filters) {
   const filteredProducts = _products.filter((p) => {
     return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
   });
+  productsDOM.innerHTML = "";
 
   //render to DOM
+  filteredProducts.forEach((item, index) => {
+    const productsDiv = document.createElement("div");
+    productsDiv.classList.add("product");
+    productsDiv.innerHTML = `
+    <div class="img-container">
+      <img src=${item.image} class="product-img" alt="p-${index}" />
+    </div>
+    <div class="product-desc">
+      <p class="product-price">${item.price} $</p>
+      <p class="product-title">${item.title}</p>
+    </div>`;
+    productsDOM.appendChild(productsDiv);
+  });
 }
 
 searchInput.addEventListener("input", (e) => {
   filters.searchItems = e.target.value;
   renderProducts(allProductsData, filters);
+});
+
+// filter based on groups
+btns.forEach((btClickEv, _, buttons) => {
+  btClickEv.onclick = (e) => {
+    const filter = e.target.dataset.filter;
+    buttons.forEach((bt) => bt.classList.toggle("active", bt === btClickEv));
+    filters.searchItems = filter;
+    renderProducts(allProductsData, filters);
+  };
 });
