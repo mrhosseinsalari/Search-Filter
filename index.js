b9d481684c1db2c63eebb53764aa68be143130d1
Hossein Salari <hosein.salari81@gmail.com>
2023-09-07 15:46:30 +0330

    JavaScript codes were rewritten

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
+// selectors
+
 const searchInput = document.querySelector("#search");
 const productsDOM = document.querySelector(".products-center");
 const btns = document.querySelectorAll(".btn");
+
+// global variable
 
 let allProductsData = [];
 
 const filters = {
   searchItems: "",
 };
+
+// functions
+
+function renderProducts(_products, _filters) {
+  const filteredProducts = _products.filter((product) =>
+    product.title.toLowerCase().includes(_filters.searchItems.toLowerCase())
+  );
+
+  productsDOM.innerHTML = "";
+
+  // render to DOM :
+  filteredProducts.forEach((item) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+    <div class="img-container">
+      <img src=${item.image} class="product-img" alt="p-${item.id}" />
+    </div>
+    <div class="product-desc">
+      <p class="product-price">${item.price} $</p>
+      <p class="product-title">${item.title}</p>
+    </div>`;
+
+    productsDOM.appendChild(productDiv);
+  });
+}
+
+// event listeners
 
 document.addEventListener("DOMContentLoaded", () => {
   axios
@@ -17,42 +50,29 @@ document.addEventListener("DOMContentLoaded", () => {
       // render products on DOM :
       renderProducts(res.data, filters);
     })
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
 });
 
-function renderProducts(_products, _filters) {
-  const filteredProducts = _products.filter((p) => {
-    return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
-  });
-  productsDOM.innerHTML = "";
-
-  //render to DOM
-  filteredProducts.forEach((item, index) => {
-    const productsDiv = document.createElement("div");
-    productsDiv.classList.add("product");
-    productsDiv.innerHTML = `
-    <div class="img-container">
-      <img src=${item.image} class="product-img" alt="p-${index}" />
-    </div>
-    <div class="product-desc">
-      <p class="product-price">${item.price} $</p>
-      <p class="product-title">${item.title}</p>
-    </div>`;
-    productsDOM.appendChild(productsDiv);
-  });
-}
-
 searchInput.addEventListener("input", (e) => {
+  btns.forEach((btn) => {
+    if (btn.classList.contains("active")) btn.classList.remove("active");
+  });
+
   filters.searchItems = e.target.value;
   renderProducts(allProductsData, filters);
 });
 
-// filter based on groups
-btns.forEach((btClickEv, _, buttons) => {
-  btClickEv.onclick = (e) => {
+// filter based on groups :
+btns.forEach((btnClickEv, _, buttons) => {
+  btnClickEv.addEventListener("click", (e) => {
     const filter = e.target.dataset.filter;
-    buttons.forEach((bt) => bt.classList.toggle("active", bt === btClickEv));
+
+    buttons.forEach((btn) =>
+      btn.classList.toggle("active", btn === btnClickEv)
+    );
+
     filters.searchItems = filter;
     renderProducts(allProductsData, filters);
-  };
+    searchInput.value = "";
+  });
 });
